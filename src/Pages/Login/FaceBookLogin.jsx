145538449265aa5762f518e008061fa77735d386
Cong-Tien <letien18102001@gmail.com>
2022-11-20 07:00:55 +0700

    fix-login-facebook

--- src/Pages/Login/FaceBookLogin.jsx
+++ src/Pages/Login/FaceBookLogin.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-// import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
-//import { loginFacebook } from '../../redux/userReducer';
+import { loginFacebook } from '../../redux/userReducer';
 
 export default function FaceBookLogin() {
     const dispatch = useDispatch();
     const navigate = useNavigate()
-    // const responseFacebook = async (response) => {
-    //     console.log(response);
-    //    const action = loginFacebook(response.accessToken);
-    //    await dispatch(action)
-    //    navigate('/profile')
-    // }
+    const responseFacebook = async (response) => {
+        console.log(response);
+       const action = loginFacebook(response.accessToken);
+       await dispatch(action)
+       navigate('/profile')
+    }
     return (
-        // <FacebookLogin
-        //     appId="2681071498696012"
-        //     autoLoad={false}
-        //     fields="name,email,picture"
-        //     callback={responseFacebook}
-        //     render={(renderProps) => (
-        //         <button className='w-100 mt-3 btn-facebook' onClick={renderProps.onClick}><i class="fab fa-facebook"></i>Continue with Facebook</button>
-        //     )}
-        // />
-        <div></div>
+        <FacebookLogin
+            appId="2681071498696012"
+            autoLoad={false}
+            fields="name,email,picture"
+            callback={responseFacebook}
+            render={(renderProps) => (
+                <button className='w-100 mt-3 btn-facebook' onClick={renderProps.onClick}><i class="fab fa-facebook"></i>Continue with Facebook</button>
+            )}
+        />
     )
 }
